ed11c650ddfd8c2636aeef50a3f210c31fddf0b6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-06-10 15:13:42 +0200

    feat: Add findFishingSpot, claimFishingSpot and consumeFishingSpot to API #16

--- common/src/main/java/net/blay09/mods/littlejoys/InternalMethodsImpl.java
+++ common/src/main/java/net/blay09/mods/littlejoys/InternalMethodsImpl.java
@@ -3,15 +3,36 @@ package net.blay09.mods.littlejoys;
 import com.mojang.serialization.MapCodec;
 import net.blay09.mods.littlejoys.api.EventCondition;
 import net.blay09.mods.littlejoys.api.InternalMethods;
+import net.blay09.mods.littlejoys.handler.FishingSpotHandler;
 import net.blay09.mods.littlejoys.recipe.condition.EventConditionRegistry;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 public class InternalMethodsImpl implements InternalMethods {
     @Override
     public <T extends EventCondition> void registerEventCondition(ResourceLocation identifier, Class<T> clazz, MapCodec<T> codec, Function<FriendlyByteBuf, T> networkDeserializer) {
         EventConditionRegistry.registerCondition(identifier, clazz, codec, networkDeserializer);
+    }
+
+    @Override
+    public Optional<BlockPos> findFishingSpot(ServerLevel level, BlockPos pos) {
+        return FishingSpotHandler.findFishingSpot(level, pos);
+    }
+
+    @Override
+    public int claimFishingSpot(ServerLevel level, BlockPos pos) {
+        return FishingSpotHandler.claimFishingSpot(level, pos);
+    }
+
+    @Override
+    public void consumeFishingSpot(@Nullable Player player, ServerLevel level, BlockPos pos) {
+        FishingSpotHandler.consumeFishingSpot(player, level, pos);
     }
 }
