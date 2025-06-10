df0491905df391f0d456d983c2988647be40b9ec
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-06-10 15:13:42 +0200

    feat: Add findFishingSpot, claimFishingSpot and consumeFishingSpot to API #16

--- common/src/main/java/net/blay09/mods/littlejoys/api/InternalMethods.java
+++ common/src/main/java/net/blay09/mods/littlejoys/api/InternalMethods.java
@@ -1,11 +1,22 @@
 package net.blay09.mods.littlejoys.api;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 public interface InternalMethods {
     <T extends EventCondition> void registerEventCondition(ResourceLocation identifier, Class<T> clazz, MapCodec<T> codec, Function<FriendlyByteBuf, T> networkDeserializer);
+
+    Optional<BlockPos> findFishingSpot(ServerLevel level, BlockPos pos);
+
+    int claimFishingSpot(ServerLevel level, BlockPos pos);
+
+    void consumeFishingSpot(@Nullable Player player, ServerLevel level, BlockPos pos);
 }
