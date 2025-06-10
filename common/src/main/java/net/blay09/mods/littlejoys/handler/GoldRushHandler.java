10162478cd947bec72fd4e4f296ba4766a95dc44
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-06-10 14:15:50 +0200

    fix: Ignore fake players in gold rush #20

--- common/src/main/java/net/blay09/mods/littlejoys/handler/GoldRushHandler.java
+++ common/src/main/java/net/blay09/mods/littlejoys/handler/GoldRushHandler.java
@@ -45,6 +45,10 @@ public class GoldRushHandler {
                 return;
             }
 
+            if (Balm.getHooks().isFakePlayer(event.getPlayer())) {
+                return;
+            }
+
             final var hasSilkTouch = event.getLevel().registryAccess().lookup(Registries.ENCHANTMENT)
                     .flatMap(it -> it.get(Enchantments.SILK_TOUCH))
                     .map(it -> EnchantmentHelper.getEnchantmentLevel(it, event.getPlayer()) > 0)
