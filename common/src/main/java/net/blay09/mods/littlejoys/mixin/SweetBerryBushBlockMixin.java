98788a0bafc458ec506440f22afe6baa6cf0a844
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-06-20 12:33:25 +0200

    feat: Add player to EventContext

--- common/src/main/java/net/blay09/mods/littlejoys/mixin/SweetBerryBushBlockMixin.java
+++ common/src/main/java/net/blay09/mods/littlejoys/mixin/SweetBerryBushBlockMixin.java
@@ -3,6 +3,7 @@ package net.blay09.mods.littlejoys.mixin;
 import net.blay09.mods.littlejoys.handler.DropRushHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -19,8 +20,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class SweetBerryBushBlockMixin {
     @Inject(method = "useWithoutItem", at = @At("RETURN"))
     public void useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit, CallbackInfoReturnable<InteractionResult> cir) {
-        if (cir.getReturnValue() == InteractionResult.CONSUME && level instanceof ServerLevel serverLevel) {
-            DropRushHandler.handleDropRushChance(serverLevel, pos, state, player);
+        if (cir.getReturnValue() == InteractionResult.CONSUME && level instanceof ServerLevel serverLevel && player instanceof ServerPlayer serverPlayer) {
+            DropRushHandler.handleDropRushChance(serverLevel, pos, state, serverPlayer);
         }
     }
 }
