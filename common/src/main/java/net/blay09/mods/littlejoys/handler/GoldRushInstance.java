69467aea04a83fa0bc1769e11e45584352a2a77e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-15 10:53:03 +0200

    fix: Keep track of player and pass them when destroying block after gold rush #26

--- common/src/main/java/net/blay09/mods/littlejoys/handler/GoldRushInstance.java
+++ common/src/main/java/net/blay09/mods/littlejoys/handler/GoldRushInstance.java
@@ -3,8 +3,10 @@ package net.blay09.mods.littlejoys.handler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootTable;
+import org.jetbrains.annotations.Nullable;
 
 public final class GoldRushInstance {
     private final BlockPos pos;
@@ -12,15 +14,17 @@ public final class GoldRushInstance {
     private final ResourceKey<LootTable> lootTable;
     private final int maxTicks;
     private final int ticksPerDrop;
+    private final Player player;
     private int ticksPassed;
     private int dropCooldownTicks;
 
-    public GoldRushInstance(BlockPos pos, BlockState initialState, ResourceKey<LootTable> lootTable, int maxTicks, int ticksPerDrop) {
+    public GoldRushInstance(BlockPos pos, BlockState initialState, ResourceKey<LootTable> lootTable, int maxTicks, int ticksPerDrop, @Nullable Player player) {
         this.pos = pos;
         this.initialState = initialState;
         this.lootTable = lootTable;
         this.maxTicks = maxTicks;
         this.ticksPerDrop = ticksPerDrop;
+        this.player = player;
     }
 
     public BlockPos getPos() {
@@ -41,6 +45,11 @@ public final class GoldRushInstance {
 
     public int getTicksPerDrop() {
         return ticksPerDrop;
+    }
+
+    @Nullable
+    public Player getPlayer() {
+        return player;
     }
 
     public int getTicksPassed() {
