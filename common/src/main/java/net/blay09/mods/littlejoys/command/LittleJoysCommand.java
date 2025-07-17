97058a38ff54e19781bdad4ed8a4b7a9bac0a37f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-07-17 12:38:20 +0200

    feat: Add /littlejoys command for triggering events #10

---
+++ common/src/main/java/net/blay09/mods/littlejoys/command/LittleJoysCommand.java
@@ -0,0 +1,131 @@
+package net.blay09.mods.littlejoys.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import net.blay09.mods.balm.api.command.BalmCommands;
+import net.blay09.mods.littlejoys.LittleJoys;
+import net.blay09.mods.littlejoys.handler.DigSpotHandler;
+import net.blay09.mods.littlejoys.handler.DropRushHandler;
+import net.blay09.mods.littlejoys.handler.FishingSpotHandler;
+import net.blay09.mods.littlejoys.handler.GoldRushHandler;
+import net.blay09.mods.littlejoys.recipe.DigSpotRecipe;
+import net.blay09.mods.littlejoys.recipe.DropRushRecipe;
+import net.blay09.mods.littlejoys.recipe.FishingSpotRecipe;
+import net.blay09.mods.littlejoys.recipe.GoldRushRecipe;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.ResourceKeyArgument;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeHolder;
+
+public class LittleJoysCommand {
+
+    private static final ResourceLocation PERMISSION_LITTLEJOYS_DIGSPOT = LittleJoys.id("command.littlejoys.digspot");
+    private static final ResourceLocation PERMISSION_LITTLEJOYS_FISHINGSPOT = LittleJoys.id("command.littlejoys.fishingspot");
+    private static final ResourceLocation PERMISSION_LITTLEJOYS_GOLDRUSH = LittleJoys.id("command.littlejoys.goldrush");
+    private static final ResourceLocation PERMISSION_LITTLEJOYS_DROPRUSH = LittleJoys.id("command.littlejoys.droprush");
+
+    private static final DynamicCommandExceptionType ERROR_UNKNOWN_RECIPE = new DynamicCommandExceptionType((arg) -> Component.translatable("recipe.notFound", arg));
+
+    @SuppressWarnings("unchecked")
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        BalmCommands.registerPermission(PERMISSION_LITTLEJOYS_DIGSPOT, 2);
+        BalmCommands.registerPermission(PERMISSION_LITTLEJOYS_FISHINGSPOT, 2);
+        BalmCommands.registerPermission(PERMISSION_LITTLEJOYS_GOLDRUSH, 2);
+        BalmCommands.registerPermission(PERMISSION_LITTLEJOYS_DROPRUSH, 2);
+
+        dispatcher.register(Commands.literal("littlejoys")
+                .then(Commands.literal("digspot")
+                        .requires(BalmCommands.requirePermission(PERMISSION_LITTLEJOYS_DIGSPOT))
+                        .then(Commands.argument("position", BlockPosArgument.blockPos())
+                                .executes(context -> {
+                                    final var level = context.getSource().getLevel();
+                                    final var player = context.getSource().getPlayerOrException();
+                                    final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                    return DigSpotHandler.createDigSpot(level, pos, player) ? 1 : 0;
+                                })
+                                .then(Commands.argument("recipe", ResourceKeyArgument.key(Registries.RECIPE))
+                                        .executes(context -> {
+                                            final var level = context.getSource().getLevel();
+                                            final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                            final var recipeHolder = ResourceKeyArgument.getRecipe(context, "recipe");
+                                            if (recipeHolder.value() instanceof DigSpotRecipe) {
+                                                DigSpotHandler.createDigSpot(level, pos, (RecipeHolder<DigSpotRecipe>) recipeHolder);
+                                                return 1;
+                                            } else {
+                                                throw ERROR_UNKNOWN_RECIPE.create(context.getArgument("recipe", ResourceKey.class));
+                                            }
+                                        }))))
+                .then(Commands.literal("fishingspot")
+                        .requires(BalmCommands.requirePermission(PERMISSION_LITTLEJOYS_FISHINGSPOT))
+                        .then(Commands.argument("position", BlockPosArgument.blockPos())
+                                .executes(context -> {
+                                    final var level = context.getSource().getLevel();
+                                    final var player = context.getSource().getPlayerOrException();
+                                    final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                    return FishingSpotHandler.createFishingSpot(level, pos, player) ? 1 : 0;
+                                })
+                                .then(Commands.argument("recipe", ResourceKeyArgument.key(Registries.RECIPE))
+                                        .executes(context -> {
+                                            final var level = context.getSource().getLevel();
+                                            final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                            final var recipeHolder = ResourceKeyArgument.getRecipe(context, "recipe");
+                                            if (recipeHolder.value() instanceof FishingSpotRecipe) {
+                                                FishingSpotHandler.createFishingSpot(level, pos, (RecipeHolder<FishingSpotRecipe>) recipeHolder);
+                                                return 1;
+                                            } else {
+                                                throw ERROR_UNKNOWN_RECIPE.create(context.getArgument("recipe", ResourceKey.class));
+                                            }
+                                        }))))
+                .then(Commands.literal("goldrush")
+                        .requires(BalmCommands.requirePermission(PERMISSION_LITTLEJOYS_GOLDRUSH))
+                        .then(Commands.argument("position", BlockPosArgument.blockPos())
+                                .executes(context -> {
+                                    final var level = context.getSource().getLevel();
+                                    final var player = context.getSource().getPlayerOrException();
+                                    final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                    GoldRushHandler.startGoldRush(level, pos, level.getBlockState(pos), player);
+                                    return 1;
+                                })
+                                .then(Commands.argument("recipe", ResourceKeyArgument.key(Registries.RECIPE))
+                                        .executes(context -> {
+                                            final var level = context.getSource().getLevel();
+                                            final var player = context.getSource().getPlayerOrException();
+                                            final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                            final var recipeHolder = ResourceKeyArgument.getRecipe(context, "recipe");
+                                            if (recipeHolder.value() instanceof GoldRushRecipe) {
+                                                GoldRushHandler.startGoldRush(level, pos, level.getBlockState(pos), player, (RecipeHolder<GoldRushRecipe>) recipeHolder);
+                                                return 1;
+                                            } else {
+                                                throw ERROR_UNKNOWN_RECIPE.create(context.getArgument("recipe", ResourceKey.class));
+                                            }
+                                        }))))
+                .then(Commands.literal("droprush")
+                        .requires(BalmCommands.requirePermission(PERMISSION_LITTLEJOYS_DROPRUSH))
+                        .then(Commands.argument("position", BlockPosArgument.blockPos())
+                                .executes(context -> {
+                                    final var level = context.getSource().getLevel();
+                                    final var player = context.getSource().getPlayerOrException();
+                                    final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                    DropRushHandler.startDropRush(level, pos, level.getBlockState(pos), player);
+                                    return 0;
+                                }).then(Commands.argument("recipe", ResourceKeyArgument.key(Registries.RECIPE))
+                                        .executes(context -> {
+                                            final var level = context.getSource().getLevel();
+                                            final var player = context.getSource().getPlayerOrException();
+                                            final var pos = BlockPosArgument.getBlockPos(context, "position");
+                                            final var recipeHolder = ResourceKeyArgument.getRecipe(context, "recipe");
+                                            if (recipeHolder.value() instanceof DropRushRecipe) {
+                                                DropRushHandler.startDropRush(level, pos, player, (RecipeHolder<DropRushRecipe>) recipeHolder);
+                                                return 1;
+                                            } else {
+                                                throw ERROR_UNKNOWN_RECIPE.create(context.getArgument("recipe", ResourceKey.class));
+                                            }
+                                        }))))
+        );
+    }
+}
