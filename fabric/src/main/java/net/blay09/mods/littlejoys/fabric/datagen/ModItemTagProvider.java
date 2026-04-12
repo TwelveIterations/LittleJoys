95ffe1a3a87516a967f87d081d2ab74164e4f171
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-12 13:33:48 +0200

    feat: Add can_trigger_goldrush tag that default events respect, to restrict gold rush to pickaxes #42

--- fabric/src/main/java/net/blay09/mods/littlejoys/fabric/datagen/ModItemTagProvider.java
+++ fabric/src/main/java/net/blay09/mods/littlejoys/fabric/datagen/ModItemTagProvider.java
@@ -3,17 +3,18 @@ package net.blay09.mods.littlejoys.fabric.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.blay09.mods.littlejoys.tag.ModItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModItemTagProvider extends FabricTagsProvider<Item> {
+public class ModItemTagProvider extends FabricTagsProvider.ItemTagsProvider {
     public ModItemTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-        super(output, Registries.ITEM, registriesFuture);
+        super(output, registriesFuture);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider lookup) {
+        valueLookupBuilder(ModItemTags.CAN_TRIGGER_GOLDRUSH).addOptionalTag(ItemTags.PICKAXES);
     }
 }
