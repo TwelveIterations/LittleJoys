f44a9b6a66fd578eb1f444fe3567a01282cda505
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-12 16:39:21 +0200

    fix: Fix gold rush breaking when mutable pos are used when triggering

--- common/src/main/java/net/blay09/mods/littlejoys/handler/GoldRushHandler.java
+++ common/src/main/java/net/blay09/mods/littlejoys/handler/GoldRushHandler.java
@@ -131,8 +131,9 @@ public class GoldRushHandler {
     }
 
     public static GoldRushInstance startGoldRush(ServerLevel level, BlockPos pos, BlockState state, ServerPlayer player, RecipeHolder<GoldRushRecipe> recipeHolder) {
+        final var immutablePos = pos.immutable();
         final var recipe = recipeHolder.value();
-        final var activeGoldRush = new GoldRushInstance(pos,
+        final var activeGoldRush = new GoldRushInstance(immutablePos,
                 state,
                 Optional.of(recipe.lootTable()),
                 (int) Math.floor(20 * recipe.seconds()),
@@ -140,8 +141,8 @@ public class GoldRushHandler {
                 player);
         player.awardStat(ModStats.goldRushesTriggered);
         ModAdvancements.awardGoldRush(player);
-        activeGoldRushes.put(level.dimension(), pos, activeGoldRush);
-        Balm.networking().sendToTracking(level, pos, new ClientboundGoldRushPacket(level.dimension(), pos, true));
+        activeGoldRushes.put(level.dimension(), immutablePos, activeGoldRush);
+        Balm.networking().sendToTracking(level, immutablePos, new ClientboundGoldRushPacket(level.dimension(), immutablePos, true));
         return activeGoldRush;
     }
 
