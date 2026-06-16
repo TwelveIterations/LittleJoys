25d8899eae090c53870246d0cbc3ad3d5c8cb7bd
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-06-16 19:47:26 +0200

    refactor(26.2): ModPoiTypeTagProvider -> FabricTagsProvider

--- fabric/src/main/java/net/blay09/mods/littlejoys/fabric/datagen/ModPoiTypeTagProvider.java
+++ fabric/src/main/java/net/blay09/mods/littlejoys/fabric/datagen/ModPoiTypeTagProvider.java
@@ -2,16 +2,16 @@ package net.blay09.mods.littlejoys.fabric.datagen;
 
 import net.blay09.mods.littlejoys.poi.ModPoiTypes;
 import net.blay09.mods.littlejoys.tag.ModPoiTypeTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModPoiTypeTagProvider extends TagsProvider<PoiType> {
-    public ModPoiTypeTagProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
+public class ModPoiTypeTagProvider extends FabricTagsProvider<PoiType> {
+    public ModPoiTypeTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
         super(output, Registries.POINT_OF_INTEREST_TYPE, provider);
     }
 
