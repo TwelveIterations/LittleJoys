240cda1e5e7acdbdda2b5cd5c29625fb7de4f822
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-15 13:02:28 +0200

    feat: Star of Serenity calms down animals

--- common/src/main/java/net/blay09/mods/littlejoys/blessing/StarOfSerenity.java
+++ common/src/main/java/net/blay09/mods/littlejoys/blessing/StarOfSerenity.java
@@ -1,15 +1,22 @@
 package net.blay09.mods.littlejoys.blessing;
 
 import net.blay09.mods.balm.platform.event.callback.LivingEntityCallback;
+import net.blay09.mods.balm.platform.event.callback.ServerTickCallback;
 import net.blay09.mods.littlejoys.sound.ModSounds;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.wolf.Wolf;
+import net.minecraft.world.phys.AABB;
 
 public class StarOfSerenity {
     public static void initialize() {
         LivingEntityCallback.Fall.Before.EVENT.register(StarOfSerenity::computeFallDamage);
+        ServerTickCallback.ServerPlayerTick.AFTER.register(StarOfSerenity::calmNearbyAnimals);
     }
 
     public static boolean preventSweetBerryBushDamage(Entity entity) {
@@ -24,6 +31,44 @@ public class StarOfSerenity {
 
         activeBlessing.consumeUse();
         return true;
+    }
+
+    private static void calmNearbyAnimals(ServerPlayer player) {
+        final var activeBlessing = BlessingManager.getActiveBlessing(player);
+        if (activeBlessing == null || !activeBlessing.is(Blessings.STAR_OF_SERENITY)) {
+            return;
+        }
+
+        final var searchArea = new AABB(player.blockPosition()).inflate(16);
+        for (final var animal : player.level().getEntitiesOfClass(Animal.class, searchArea, Entity::isAlive)) {
+            if (calmPanic(animal)) {
+                activeBlessing.consumeUse();
+            }
+            if (calmAggressiveWolf(animal)) {
+                activeBlessing.consumeUse();
+            }
+        }
+    }
+
+    private static boolean calmPanic(Animal animal) {
+        if (animal instanceof PathfinderMob mob && !animal.isOnFire() && animal.getLastDamageSource() != null) {
+            final var lastDamageSource = animal.getLastDamageSource();
+            if (lastDamageSource.is(DamageTypeTags.PANIC_CAUSES) || lastDamageSource.is(DamageTypeTags.PANIC_ENVIRONMENTAL_CAUSES)) {
+                mob.getNavigation().stop();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean calmAggressiveWolf(Animal animal) {
+        if (animal instanceof Wolf wolf && wolf.isAngry()) {
+            wolf.stopBeingAngry();
+            wolf.setAggressive(false);
+            wolf.getNavigation().stop();
+            return true;
+        }
+        return false;
     }
 
     private static float computeFallDamage(LivingEntity entity, float fallDamage) {
