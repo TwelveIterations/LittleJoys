ed1f1516158da7854e7facf95e91eff7fc2e3adf
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-02 21:32:29 +0200

    refactor(26.3): Replace above_fluid_source with Shogi effects

--- common/src/main/java/net/blay09/mods/littlejoys/recipe/condition/LittleJoysRules.java
+++ common/src/main/java/net/blay09/mods/littlejoys/recipe/condition/LittleJoysRules.java
@@ -1,17 +1,8 @@
 package net.blay09.mods.littlejoys.recipe.condition;
 
-import com.mojang.datafixers.util.Either;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.blay09.mods.littlejoys.recipe.FluidIngredient;
 import net.blay09.mods.shogi.Shogi;
-import net.blay09.mods.shogi.context.ShogiContext;
 import net.blay09.mods.shogi.effect.ShogiEffect;
 import net.blay09.mods.shogi.scope.ShogiScope;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import java.util.List;
 
@@ -20,8 +11,6 @@ import static net.blay09.mods.littlejoys.LittleJoys.id;
 public final class LittleJoysRules {
     public static final ShogiScope EVENT_CONDITIONS = Shogi.scope(id("event_conditions"), scope -> {
         scope.setDefaultNamespaces(List.of("littlejoys", "shogi"));
-        scope.registerEffect(AboveFluidSource.IDENTIFIER, AboveFluidSource.MAP_CODEC);
-        scope.registerEffect(AboveState.IDENTIFIER, AboveState.MAP_CODEC, List.of("state"));
     });
     public static final ShogiEffect<Boolean> UNSYNCED_EVENT_CONDITION = ShogiEffect.simple(id("unsynced_event_condition"), () -> false);
 
@@ -30,46 +19,5 @@ public final class LittleJoysRules {
 
     public static void initialize() {
         EVENT_CONDITIONS.identifier();
-    }
-
-    public record AboveFluidSource(FluidIngredient fluid, boolean allowWaterlogged) implements ShogiEffect<Boolean> {
-        public static final Identifier IDENTIFIER = id("above_fluid_source");
-        public static final MapCodec<AboveFluidSource> MAP_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-                FluidIngredient.CODEC.fieldOf("fluid").orElse(FluidIngredient.WATER).forGetter(AboveFluidSource::fluid),
-                Codec.BOOL.optionalFieldOf("allowWaterlogged", false).forGetter(AboveFluidSource::allowWaterlogged)
-        ).apply(instance, AboveFluidSource::new));
-
-        @Override
-        public Either<Boolean, Throwable> apply(ShogiContext context) {
-            final var groundState = context.requireLevel().getBlockState(context.requireBlockPos().below());
-            if (!allowWaterlogged && groundState.getValueOrElse(BlockStateProperties.WATERLOGGED, false)) {
-                return Either.left(false);
-            }
-
-            final var fluidState = groundState.getFluidState();
-            return Either.left(fluid.test(fluidState) && fluidState.isSource());
-        }
-
-        @Override
-        public Identifier identifier() {
-            return IDENTIFIER;
-        }
-    }
-
-    public record AboveState(BlockState state) implements ShogiEffect<Boolean> {
-        public static final Identifier IDENTIFIER = id("above_state");
-        public static final MapCodec<AboveState> MAP_CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-                BlockState.CODEC.fieldOf("state").forGetter(AboveState::state)
-        ).apply(instance, AboveState::new));
-
-        @Override
-        public Either<Boolean, Throwable> apply(ShogiContext context) {
-            return Either.left(context.requireLevel().getBlockState(context.requireBlockPos().below()).equals(state));
-        }
-
-        @Override
-        public Identifier identifier() {
-            return IDENTIFIER;
-        }
     }
 }
